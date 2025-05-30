c9ef6818f4097b9feb7d4a2943ba1f068d8577e3
Wally Darius <alidouwrm@gmail.com>
2025-05-30 16:14:39 +0000

    fonts

--- ProjetHopi/settings.py
+++ ProjetHopi/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.template.context_processors import media
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -73,10 +75,6 @@ WSGI_APPLICATION = 'ProjetHopi.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'projet_hopi',
